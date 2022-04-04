50d44849f6179f35d31bec84168fb498750ce2c7
eliasg4ever <eliasg4ever@gmail.com>
2022-04-03 20:25:47 -0400

    initial Set of CircleCI
    initial Set of CircleCI

---
+++ SimpleHello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
